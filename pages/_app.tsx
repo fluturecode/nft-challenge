7994e68c81e94e793eab41e8419ac2676057370a
Elizabeth Eidelson <fluturecode@gmail.com>
2022-04-01 09:59:42 -0400

    added thirdweb provider to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    //Using Rinkeby instead of Mainnet, to test network on Ethereum
+    <ThirdwebProvider desiredChainId={ChainId.Rinkeby}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  )
 }
 
 export default MyApp

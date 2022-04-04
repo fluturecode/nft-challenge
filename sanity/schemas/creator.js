e1f897051b73be5cba575e64eb5efdefe88e1333
Elizabeth Eidelson <fluturecode@gmail.com>
2022-04-03 20:36:25 -0400

    updated schema with collection and creator

--- sanity/schemas/creator.js
+++ sanity/schemas/creator.js
@@ -9,6 +9,11 @@ export default {
       type: 'string',
     },
     {
+      name: 'address',
+      title: 'Address',
+      type: 'string',
+    },
+    {
       name: 'slug',
       title: 'Slug',
       type: 'slug',
